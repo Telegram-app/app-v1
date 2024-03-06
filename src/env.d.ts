91f3aafc101c4d6cfa0fd53ba07ae0a75cc732d1
Felaner <kirill.deykun1@gmail.com>
2024-03-06 15:51:26 +0700

    auction page 60%

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 interface ImportMetaEnv {
-  VITE_BASE_URL: "http://localhost:5173";
+  // VITE_BASE_URL: "http://localhost:5173";
+  VITE_BASE_URL: "https://main--eloquent-youtiao-dc31f9.netlify.app";
   // more env variables...
 }
