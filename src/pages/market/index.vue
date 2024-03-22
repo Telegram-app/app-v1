20ad03f5cada379181c27d7ea4b4437b5dc60809
Felaner <kirill.deykun1@gmail.com>
2024-03-22 14:57:52 +0700

    slides

--- src/pages/market/index.vue
+++ src/pages/market/index.vue
@@ -317,7 +317,7 @@ export default defineComponent({
         line-height: 1;
       }
       
-      &:hover {
+      &:focus {
         img {
           -webkit-box-shadow: 0px 0px 5px 5px rgba(0,0,0,0.75);
           -moz-box-shadow: 0px 0px 5px 5px rgba(0,0,0,0.75);
