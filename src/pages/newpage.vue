a1c712dd268a93c08d06e51cd7f8d819d9e94bfc
Felaner <kirill.deykun1@gmail.com>
2024-05-30 17:07:22 +0700

    scroll snap on newpage

--- src/pages/newpage.vue
+++ src/pages/newpage.vue
@@ -535,7 +535,7 @@ html {
     border: 4px solid #f3f3f3; /* Light grey */
     border-top: 4px solid #3498db; /* Blue */
     border-radius: 50%;
-    margin-bottom: 15px;
+    margin: 15px 0;
     width: 40px;
     height: 40px;
     
